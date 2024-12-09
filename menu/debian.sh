fc4240c6d4a8cf0140331778049dc88c473b864f
kashy254 <kashmanuu361@gmail.com>
2024-12-09 18:19:31 +0000

    Update debian.sh

--- menu/debian.sh
+++ menu/debian.sh
@@ -2,7 +2,7 @@
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition 1.0
 # Auther  :   Emmkash
-# Emmkash tecg
+# Emmkash tech
 # (C) Copyright 2024
 # =========================================
 # pewarna hidup
